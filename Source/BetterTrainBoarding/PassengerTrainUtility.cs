f7491722881e2598c28fba8244b9d60ae4768cd2
Vectorial1024 <vincentwwy168@gmail.com>
2022-03-19 11:15:48 +0800

    Name variables for better readability

--- Source/BetterTrainBoarding/PassengerTrainUtility.cs
+++ Source/BetterTrainBoarding/PassengerTrainUtility.cs
@@ -27,20 +27,20 @@ namespace BetterTrainBoarding
         public static bool GetClosestTrailer(ushort vehicleID, Vector3 position, out ushort trailerID)
 		{
 			VehicleManager instance = Singleton<VehicleManager>.instance;
-			float num = 1E+10f;
+			float distanceToClosestTrailer = 1E+10f;
 			trailerID = 0;
-			int num2 = 0;
+			int trailerIterationGuard = 0;
 			while (vehicleID != 0)
 			{
 				Frame lastFrameData = instance.m_vehicles.m_buffer[vehicleID].GetLastFrameData();
-				float num3 = Vector3.SqrMagnitude(position - lastFrameData.m_position);
-				if (num3 < num)
+				float currentDistance = Vector3.SqrMagnitude(position - lastFrameData.m_position);
+				if (currentDistance < distanceToClosestTrailer)
 				{
-					num = num3;
+					distanceToClosestTrailer = currentDistance;
 					trailerID = vehicleID;
 				}
 				vehicleID = instance.m_vehicles.m_buffer[vehicleID].m_trailingVehicle;
-				if (++num2 > 16384)
+				if (++trailerIterationGuard > 16384)
 				{
 					CODebugBase<LogChannel>.Error(LogChannel.Core, "Invalid list detected!\n" + Environment.StackTrace);
 					break;
