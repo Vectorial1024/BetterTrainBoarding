673ba1a1fbfaa186eb099f4ab98259bc08893a5e
Vincent Wong <vincentwwy168@gmail.com>
2023-05-07 21:53:43 +0800

    Extract common code to new util func

---
+++ Source/BetterTrainBoarding/CommonActionBetterBoarding.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace BetterTrainBoarding
+{
+    public class CommonActionBetterBoarding
+    {
+        public static bool HandleBetterBoarding(ushort vehicleID, ref Vehicle data, ushort currentStop, ushort nextStop)
+        {
+            bool isFull;
+            List<PassengerTrainUtility.CompartmentInfo> analysis;
+            PassengerTrainUtility.AnalyzeTrain(vehicleID, out isFull, out analysis);
+            if (!isFull)
+            {
+                // this gets triggered 99% of the time, but eh who cares.
+                PassengerTrainUtility.SensiblyLoadPassengers(vehicleID, currentStop, nextStop, analysis);
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- Source/BetterTrainBoarding/Prefix_TrolleybusAI_LoadPassengers.cs
+++ Source/BetterTrainBoarding/Prefix_TrolleybusAI_LoadPassengers.cs
@@ -1,5 +1,4 @@
 ﻿using HarmonyLib;
-using System.Collections.Generic;
 
 namespace BetterTrainBoarding
 {
@@ -12,16 +11,7 @@ namespace BetterTrainBoarding
         [HarmonyPrefix]
         public static bool PreFix(ushort vehicleID, ref Vehicle data, ushort currentStop, ushort nextStop)
         {
-            bool isFull;
-            List<PassengerTrainUtility.CompartmentInfo> analysis;
-            PassengerTrainUtility.AnalyzeTrain(vehicleID, out isFull, out analysis);
-            if (!isFull)
-            {
-                // this gets triggered 99% of the time, but eh who cares.
-                PassengerTrainUtility.SensiblyLoadPassengers(vehicleID, currentStop, nextStop, analysis);
-                return false;
-            }
-            return true;
+            return CommonActionBetterBoarding.HandleBetterBoarding(vehicleID, ref data, currentStop, nextStop);
         }
     }
 }
