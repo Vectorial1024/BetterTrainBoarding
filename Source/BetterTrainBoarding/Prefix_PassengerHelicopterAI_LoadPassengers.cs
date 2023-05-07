501bde865daa5e0437748de1258895fa7cce836e
Vincent Wong <vincentwwy168@gmail.com>
2023-05-07 22:36:31 +0800

    More transport types included in Better Boarding

---
+++ Source/BetterTrainBoarding/Prefix_PassengerHelicopterAI_LoadPassengers.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace BetterTrainBoarding
+{
+    [HarmonyPatch(typeof(PassengerHelicopterAI))]
+    [HarmonyPatch("LoadPassengers", MethodType.Normal)]
+    // need to execute after our other mod, Express Bus Services
+    [HarmonyAfter(new string[] { PatchController.ExpressBusServicesHarmonyID })]
+    public class Prefix_PassengerHelicopterAI_LoadPassengers
+    {
+        [HarmonyPrefix]
+        public static bool PreFix(ushort vehicleID, ref Vehicle data, ushort currentStop, ushort nextStop)
+        {
+            return CommonActionBetterBoarding.HandleBetterBoarding(vehicleID, ref data, currentStop, nextStop);
+        }
+    }
+}
