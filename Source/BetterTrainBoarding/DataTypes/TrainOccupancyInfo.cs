47367afd8a20b21ce5fe2bd0b472ba16dbad537d
Vincent Wong <vincentwwy168@gmail.com>
2025-06-04 16:54:55 +0800

    Create TrainOccupancyInfo

---
+++ Source/BetterTrainBoarding/DataTypes/TrainOccupancyInfo.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using ColossalFramework;
+
+namespace BetterTrainBoarding.DataTypes
+{
+    public class TrainOccupancyInfo
+    {
+        public ushort FirstVehicleID { get; private set; }
+
+        private Dictionary<ushort, VehicleOccupancyInfo> _compartmentOccupancy = new Dictionary<ushort, VehicleOccupancyInfo>();
+
+        public TrainOccupancyInfo(ushort vehicleID)
+        {
+            // find the vehicle ID of the first vehicle first
+            SetFirstVehicleID(vehicleID);
+
+            // then, iterate through the train to find per-vehicle occupancy
+            var vehicleManager = Singleton<VehicleManager>.instance;
+            var currentVehicleID = FirstVehicleID;
+            while (true)
+            {
+                var currentVehicleInstance = vehicleManager.m_vehicles.m_buffer[currentVehicleID];
+                _compartmentOccupancy[currentVehicleID] = new VehicleOccupancyInfo(currentVehicleID);
+                var nextVehicleID = currentVehicleInstance.m_trailingVehicle;
+                if (nextVehicleID == 0)
+                {
+                    break;
+                }
+                currentVehicleID = nextVehicleID;
+            }
+        }
+
+        private void SetFirstVehicleID(ushort vehicleID)
+        {
+            var currentVehicleID = vehicleID;
+            var vehicleManager = Singleton<VehicleManager>.instance;
+            var currentVehicleInstance = vehicleManager.m_vehicles.m_buffer[currentVehicleID];
+            while (currentVehicleInstance.m_leadingVehicle != 0)
+            {
+                // move 1 vehicle to the front
+                currentVehicleID = currentVehicleInstance.m_leadingVehicle;
+                currentVehicleInstance = vehicleManager.m_vehicles.m_buffer[currentVehicleID];
+            }
+            // we are at the front
+            FirstVehicleID = currentVehicleID;
+        }
+    }
+}
